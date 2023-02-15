eb50954d2a47d7c3b084bab4d35226c84357c626
sdfields <spencerdfields@gmail.com>
2023-02-15 15:03:40 -0600

    added new routes for user and expense js to public

--- controllers/api/expense-routes.js
+++ controllers/api/expense-routes.js
@@ -1,20 +1,36 @@
-const router = require('express').Router();
-const db = require('../../config/connection');
-const { Expenses } = require('../../models');
+const router = require("express").Router();
+const db = require("../../config/connection");
+const { Expenses } = require("../../models");
 
 // /api/users
-router.get('/', (req, res) => {
-	// console.log('button hit');
+router.get("/", (req, res) => {
+  // console.log('button hit');
 
-	try {
-		res.render('expenseForm', {});
-		// const query = db.query('SELECT * FROM Expenses');
-		//where, userName = '${userName}' AND userEmail = '${userEmail}';
+  try {
+    res.render("expenseForm", {});
+    // const query = db.query('SELECT * FROM Expenses');
+    //where, userName = '${userName}' AND userEmail = '${userEmail}';
 
-		console.log('/api/user-routes was hit');
-	} catch (err) {
-		res.status(500).json(err);
-	}
+    console.log("/api/user-routes was hit");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const transaction = await Expenses.create({
+      expense_name: req.expense_name,
+      date: req.date,
+      category: req.category,
+      pay_to: req.pay_to,
+      payment_amount: req.body.payment_amount,
+      user_id: req.session.user_id,
+    });
+    res.json(transaction);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const loginRoutes = require('./login-routes');
 const userRoutes = require('./user-routes');
 const expenseRoutes = require('./expense-routes');
 
-router.use('/', loginRoutes);
 router.use('/users', userRoutes);
 router.use('/expenseForm', expenseRoutes);
 
