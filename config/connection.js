e542e1c59fc46d32e0db94982bead808572b7ebc
Chris Cline <115193125+ccline29@users.noreply.github.com>
2023-02-18 07:54:46 -0600

    config

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = new Sequelize(
+const sequelize = new Sequelize(
    // process.env.DB_NAME,
    // process.env.DB_USERNAME,
    // process.env.DB_PASSWORD,
