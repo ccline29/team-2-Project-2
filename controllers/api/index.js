93d63af7ded7056ce12589aaad4075726bcf8c2c
Chris Cline <115193125+ccline29@users.noreply.github.com>
2023-02-21 19:45:48 -0600

    api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const expensesRoutes = require('./api/expensesRoutes');
+const expensesRoutes = require('/Users/chriscline/bootcamp/team-2-Project-2/controllers/api/expenseRoutes.js');
 
 // route /api/users and
 router.use('/users', userRoutes);
